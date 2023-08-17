70a8fffc067af07514c806c9bbfb5dc245a77305
Michał Łata <mikepatch.code@gmail.com>
2023-08-17 21:03:52 +0200

    feat: Improve Navbar component, Add svg icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,46 @@
+import styles from "./navbar.module.scss";
+import { ReactComponent as GeneralIcon } from "@icons/general_icon.svg";
+import { ReactComponent as HistoryIcon } from "@icons/history_icon.svg";
+import { ReactComponent as ChartsIcon } from "@icons/charts_icon.svg";
+import Logo from "./Logo";
+
 export default function Navbar() {
   const navItems = [
     {
       id: "general",
       label: "Ogólne",
-      href: "/general",
+      href: "/",
+      Icon: GeneralIcon,
     },
     {
       id: "history",
       label: "Historia",
       href: "/history",
+      Icon: HistoryIcon,
     },
     {
       id: "charts",
       label: "Wykresy",
       href: "/charts",
+      Icon: ChartsIcon,
     },
   ];
 
+  const renderNavItems = navItems.map(({ id, label, href, Icon }) => (
+    <li key={id}>
+      <a className={styles.navLink} href={href}>
+        <Icon />
+        {label}
+      </a>
+    </li>
+  ));
+
   return (
-    <nav>
-      <ul>
-        {navItems.map(({ id, label, href }) => (
-          <li key={id}>
-            <a href={href}>{label}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className={styles.wrapper}>
+      <nav className={styles.navBar}>
+        <Logo />
+        <ul className={styles.navList}>{renderNavItems}</ul>
+      </nav>
+    </div>
   );
 }
