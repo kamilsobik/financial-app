4dfeb1696e5ffda85ed6c8c28804184f640e638d
Michał Łata <mikepatch.code@gmail.com>
2023-08-18 09:08:24 +0200

    fix: Change the assets directory

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@styles": path.resolve(__dirname, "./src/styles"),
-      "@icons": path.resolve(__dirname, "./public/icons"),
+      "@icons": path.resolve(__dirname, "./src/assets/icons"),
+      "@images": path.resolve(__dirname, "./src/assets/images"),
     },
   },
   plugins: [svgr(), react()],
