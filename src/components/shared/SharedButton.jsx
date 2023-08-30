551643a8f1285d589fd91308ca9b04edcec6952f
Kamil Sobik <kamil.sobik@gmail.com>
2023-08-30 16:43:36 +0200

    Feat: Add shared button -posible to edit label, onClick actions and UI with props (tailwind classes)

---
+++ src/components/shared/SharedButton.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+
+const SharedButton = ({
+  label,
+  onClick,
+  backgroundColor,
+  hoverColor,
+  textColor,
+  fontSize,
+  width,
+  height,
+}) => {
+  const buttonStyle = {
+    backgroundColor: backgroundColor || "bg-blue-500",
+    hoverColor: hoverColor || "hover:bg-blue-700",
+    color: textColor || "text-white",
+    fontSize: fontSize || "text-base",
+    width: width || "w-auto",
+    height: height || "h-auto",
+  };
+
+  return (
+    <button
+      className={`font-bold py-2 px-4 rounded ${buttonStyle.backgroundColor} ${buttonStyle.hoverColor} ${buttonStyle.color} ${buttonStyle.fontSize} ${buttonStyle.width} ${buttonStyle.height}`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+SharedButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  backgroundColor: PropTypes.string,
+  hoverColor: PropTypes.string,
+  textColor: PropTypes.string,
+  fontSize: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
+
+export default SharedButton;
