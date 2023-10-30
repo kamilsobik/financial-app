0feca6d7dc2c40966bf1090d6a21823934ea2150
Kamil Sobik <kamil.sobik@gmail.com>
2023-10-30 21:41:44 +0100

    Fix: Add index with export components

---
+++ src/icons/index.js
@@ -0,0 +1,23 @@
+import AmusementIcon from "./AmusementIcon";
+import BasketIcon from "./BascetIcon";
+import CarIcon from "./CarIcon";
+import ChargedIcon from "./ChargedIcon";
+import EducationIcon from "./EducationIcon";
+import HealthCareIcon from "./HealthCareIcon";
+import OtherIcon from "./OtherIcon";
+import RestaurantIcon from "./RestaurantIcon";
+import TransportIcon from "./TransportIcon";
+import TravelsIcon from "./TravelsIcon";
+
+export default {
+  AmusementIcon,
+  BasketIcon,
+  CarIcon,
+  ChargedIcon,
+  EducationIcon,
+  HealthCareIcon,
+  OtherIcon,
+  RestaurantIcon,
+  TransportIcon,
+  TravelsIcon,
+};
